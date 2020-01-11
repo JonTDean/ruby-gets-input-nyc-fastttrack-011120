dc345db3b1dd392d2a828cc86c4138ae908d03d2
Jonathan T Dean <jonathantomdean@gmail.com>
2020-01-11 18:35:43 +0000

    Automatically backed up by Learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,8 +1,8 @@
 def greeting(name)
   
   puts "Please enter your name so that we can greet you more personally:"
-  name = gets.strip
   
+  name = gets.strip
   
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
   
